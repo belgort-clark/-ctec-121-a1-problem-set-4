ecff4af2b3ca8704fe15368e21fd0dde3a8ba889
belgort-clark <belgort@clark.edu>
2021-01-19 16:20:49 -0800

    Initial commit

---
+++ problem-set-4-problem-4.py
@@ -0,0 +1,30 @@
+# YOUR NAME
+# CTEC 121
+# Module 4 / Problem Set 4
+# Problem 4 (25 points)
+
+"""
+DRAW A FACE
+===========
+
+Develop a program that draws some sort of substantial face that 
+must include:
+    
+    - two eyes
+    - a nose
+    - a mouth with some teeth
+    - two ears 
+    - some hair
+
+You will find faces that were drawn by students in prior classes 
+in a file named faces.pdf. You cannot open the PDF file in Visual Studio Code.
+"""
+
+import graphics
+
+
+def main():
+    pass  # remove this line
+
+
+main()
